a458a1e79c04cfc9a762b9ba8fcc1a1262af4f1d
fazevaib <vaibhavvats123@gmail.com>
2019-09-28 02:18:03 +0530

    fixed carousel effect and added project

--- assets/js/carouseleffect.js
+++ assets/js/carouseleffect.js
@@ -21,7 +21,7 @@ TxtRotate.prototype.tick = function () {
     this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
     var that = this;
-    var delta = 200 - Math.random() * 100;
+    var delta = 120 - Math.random() * 100;
 
     if (this.isDeleting) {
         delta /= 2;
@@ -33,7 +33,7 @@ TxtRotate.prototype.tick = function () {
     } else if (this.isDeleting && this.txt === '') {
         this.isDeleting = false;
         this.loopNum++;
-        delta = 500;
+        delta = 200;
     }
 
     setTimeout(function () {
